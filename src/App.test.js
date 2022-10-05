c6619cdd885f9ea950d7ec0943adcad1d629534b
niyoh55 <niyohedwyn21@gmail.com>
2022-10-06 06:27:11 +0800

    added test

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('check if name is included', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/niyoh/i);
+  expect(linkElement).toBeInTheDocument();
+});
